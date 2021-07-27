d72a1eebd93a6bee26f3cd4b4760fdecf04a9627
Octavian Popa <octavian.popa@sprintsoft.ro>
2021-07-27 08:44:55 +0200

    add necesary files

---
+++ lang/en/lang.php
@@ -0,0 +1,10 @@
+<?php return [
+    'plugin' => [
+        'name' => 'Lista de promotii',
+        'description' => ''
+    ],
+    'promotionslist'     => [
+        'name'         => 'Lista de promotii',
+        'list_title'   => 'Promotii',
+    ],
+];
